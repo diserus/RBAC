238c36cfc75cfe725252cdbc8b43142c01bfb700
diserus <prozorenko24@gmail.com>
2026-02-23 16:03:46 +0700

    Added RoleFilters and a little refactoring

--- src/UserFilter.java
+++ src/UserFilter.java
@@ -1,5 +1,5 @@
 @FunctionalInterface
-interface UserFilter {
+public interface UserFilter {
     boolean test(User user);
 
     default UserFilter and(UserFilter other) {
@@ -8,29 +8,5 @@ interface UserFilter {
 
     default UserFilter or(UserFilter other) {
         return user -> this.test(user) || other.test(user);
-    }
-}
-
-class UserFilters {
-    public static UserFilter byUsername(String username) {
-        return user -> user.username().equals(username);
-    }
-
-    public static UserFilter byUsernameContains(String substring) {
-        return user -> user.username().toLowerCase()
-                .contains(substring.toLowerCase());
-    }
-
-    public static UserFilter byEmail(String email) {
-        return user -> user.email().equals(email);
-    }
-
-    public static UserFilter byEmailDomain(String domain) {
-        return user -> user.email().endsWith(domain);
-    }
-
-    public static UserFilter byFullNameContains(String substring) {
-        return user -> user.fullName().toLowerCase()
-                .contains(substring.toLowerCase());
     }
 }
