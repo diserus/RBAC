ecfac70761e4cd01ef47bb48ad69afcc156c4a0c
diserus <prozorenko24@gmail.com>
2026-04-13 19:02:15 +0700

    Add expired assignment maintenance scan

--- src/main/java/AssignmentManager.java
+++ src/main/java/AssignmentManager.java
@@ -127,6 +127,18 @@ public class AssignmentManager implements Repository<RoleAssignment> {
         temp.extend(newExpirationDate);
     }
 
+    public int deactivateExpiredTemporaryAssignments() {
+        List<TemporaryAssignment> expiredAssignments = assignments.values().stream()
+                .filter(TemporaryAssignment.class::isInstance)
+                .map(TemporaryAssignment.class::cast)
+                .filter(assignment -> !assignment.isDeactivated())
+                .filter(TemporaryAssignment::isExpired)
+                .collect(Collectors.toList());
+
+        expiredAssignments.forEach(TemporaryAssignment::deactivate);
+        return expiredAssignments.size();
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
